2cb64f82e26cdde8ea08c16e10c74182afb0feba
lkuffo <lxkr@cwi.nl>
2025-05-27 23:24:29 +0200

    Jaccard LUT hybrid

--- include/avx512_jaccard.cpp
+++ include/avx512_jaccard.cpp
@@ -188,16 +188,15 @@ void jaccard_b256_vpopcntq_vpshufb_pdx(uint8_t const *first_vector, uint8_t cons
 
         for (size_t i = 0; i < 8; i++){ // 256 uint8_t values
             __m256i second = _mm256_loadu_epi8((__m256i const*)(second_vector));
-//
-//            // Getting nibbles from data
-//            __m256i second_low = _mm256_and_si256(second, low_mask);
-//            __m256i second_high = _mm256_and_si256(_mm256_srli_epi16(second, 4), low_mask);
-//
-//            __m256i intersection = _mm256_add_epi8(
-//                _mm256_shuffle_epi8(lut_intersection_low, second_low),
-//                _mm256_shuffle_epi8(lut_intersection_high, second_high)
-//            );
-            __m256i intersection = _mm256_popcnt_epi8(_mm256_and_epi64(first, second));
+
+            // Getting nibbles from data
+            __m256i second_low = _mm256_and_si256(second, low_mask);
+            __m256i second_high = _mm256_and_si256(_mm256_srli_epi16(second, 4), low_mask);
+
+            __m256i intersection = _mm256_add_epi8(
+                _mm256_shuffle_epi8(lut_intersection_low, second_low),
+                _mm256_shuffle_epi8(lut_intersection_high, second_high)
+            );
 
             __m256i union_ = _mm256_popcnt_epi8(_mm256_or_epi64(first, second));
 
