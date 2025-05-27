6255fe26abb614827ba9fd96f258d6e9f5a5877b
lkuffo <lxkr@cwi.nl>
2025-05-27 21:05:01 +0200

    Jaccard LUT+PDX implementation

--- include/avx512_jaccard.cpp
+++ include/avx512_jaccard.cpp
@@ -133,10 +133,13 @@ void jaccard_b256_vpshufb_pdx(uint8_t const *first_vector, uint8_t const *second
         _mm256_storeu_si256((__m256i *)(unions_tmp + (i * 32)), unions_result[i]);
     }
     for (size_t i = 0; i < 256; i++){
-        std::cout << "i: " << i << "\n";
-        std::cout << "--> union: " << +unions_tmp[i] << "\n";
-        std::cout << "--> intersect: " << +intersections_tmp[i] << "\n";
-        distances_tmp[i] = (unions_tmp[i] != 0) ? 1 - (float)intersections_tmp[i] / (float)unions_tmp[i] : 1.0f;
+        if (unions_tmp[i] == 0 && intersections_tmp[i] == 0){
+            // Dumb (?) overflow when vectors are exactly the same
+            // Im not sure why it doesn't happen in the other PDX kernel
+            distances_tmp[i] = 0.0f;
+        } else {
+            distances_tmp[i] = (unions_tmp[i] != 0) ? 1 - (float)intersections_tmp[i] / (float)unions_tmp[i] : 1.0f;
+        }
     }
 }
 
