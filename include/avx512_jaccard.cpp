0818bcc7108f7b7ac95efa7af8ef93410fcf0a45
lkuffo <lxkr@cwi.nl>
2025-05-28 00:28:58 +0200

    Jaccard LUT hybrid

--- include/avx512_jaccard.cpp
+++ include/avx512_jaccard.cpp
@@ -187,10 +187,11 @@ void jaccard_b256_vpopcntq_vpshufb_pdx(uint8_t const *first_vector, uint8_t cons
             __m256i second_low = _mm256_and_si256(second, low_mask);
             //__m256i second_high = _mm256_and_si256(_mm256_srli_epi16(second, 4), low_mask);
 
-            __m256i intersection = _mm256_add_epi8(
-                _mm256_shuffle_epi8(lut_intersection_low, second_low),
-                _mm256_shuffle_epi8(lut_intersection_high, second_low)
-            );
+            //__m256i intersection = _mm256_add_epi8(
+                //_mm256_shuffle_epi8(lut_intersection_low, second_low),
+                //_mm256_shuffle_epi8(lut_intersection_high, second_low)
+            //);
+            __m256i intersection = _mm256_shuffle_epi8(lut_intersection_low, second_low);
 
             __m256i union_ = _mm256_popcnt_epi8(_mm256_or_epi64(first, second));
 
