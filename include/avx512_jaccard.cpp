601afce9a60049d05e1759b5547788f6beed4d5c
lkuffo <lxkr@cwi.nl>
2025-05-28 00:10:55 +0200

    Jaccard LUT hybrid

--- include/avx512_jaccard.cpp
+++ include/avx512_jaccard.cpp
@@ -177,9 +177,8 @@ void jaccard_b256_vpopcntq_vpshufb_pdx(uint8_t const *first_vector, uint8_t cons
         // Choose lookup tables
         __m256i lut_intersection_high = m256_intersection_lookup_tables[first_high];
         __m256i lut_intersection_low = m256_intersection_lookup_tables[first_low];
-//        __m256i lut_union_high = m256_union_lookup_tables[first_high];
-//        __m256i lut_union_low = m256_union_lookup_tables[first_low];
 
+#pragma unroll
         for (size_t i = 0; i < 8; i++){ // 256 uint8_t values
             __m256i second = _mm256_loadu_epi8((__m256i const*)(second_vector));
 
