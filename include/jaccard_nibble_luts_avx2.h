47deaf4007fa759cb2fd2ff54944322f95852ac7
lkuffo <lxkr@cwi.nl>
2025-05-28 16:50:58 +0200

    Trying to find the bottleneck

--- include/jaccard_nibble_luts_avx2.h
+++ include/jaccard_nibble_luts_avx2.h
@@ -5,176 +5,176 @@
 ///////////////////////////////
 ///////////////////////////////
 
-static const __m256i m256_intersection_lut_0  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_0  = _mm256_set_epi8(
     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 );
 
-static const __m256i m256_intersection_lut_1  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_1  = _mm256_set_epi8(
     1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,
     1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0
 );
 
-static const __m256i m256_intersection_lut_2  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_2  = _mm256_set_epi8(
     1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,
     1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0
 );
 
-static const __m256i m256_intersection_lut_3  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_3  = _mm256_set_epi8(
     2,1,1,0,2,1,1,0,2,1,1,0,2,1,1,0,
     2,1,1,0,2,1,1,0,2,1,1,0,2,1,1,0
 );
 
-static const __m256i m256_intersection_lut_4  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_4  = _mm256_set_epi8(
     1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,
     1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0
 );
 
-static const __m256i m256_intersection_lut_5  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_5  = _mm256_set_epi8(
     2,1,2,1,1,0,1,0,2,1,2,1,1,0,1,0,
     2,1,2,1,1,0,1,0,2,1,2,1,1,0,1,0
 );
 
-static const __m256i m256_intersection_lut_6  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_6  = _mm256_set_epi8(
     2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1,
     2,2,1,1,2,2,1,1,2,2,1,1,2,2,1,1
 );
 
-static const __m256i m256_intersection_lut_7  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_7  = _mm256_set_epi8(
     3,2,2,1,3,2,2,1,3,2,2,1,3,2,2,1,
     3,2,2,1,3,2,2,1,3,2,2,1,3,2,2,1
 );
 
-static const __m256i m256_intersection_lut_8  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_8  = _mm256_set_epi8(
     1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
     1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0
 );
 
-static const __m256i m256_intersection_lut_9  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_9  = _mm256_set_epi8(
     2,1,2,1,2,1,2,1,1,0,1,0,1,0,1,0,
     2,1,2,1,2,1,2,1,1,0,1,0,1,0,1,0
 );
 
-static const __m256i m256_intersection_lut_10 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_10 = _mm256_set_epi8(
     2,2,1,1,2,2,1,1,1,1,0,0,1,1,0,0,
     2,2,1,1,2,2,1,1,1,1,0,0,1,1,0,0
 );
 
-static const __m256i m256_intersection_lut_11 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_11 = _mm256_set_epi8(
     3,2,2,1,3,2,2,1,2,1,1,0,3,2,2,1,
     3,2,2,1,3,2,2,1,2,1,1,0,3,2,2,1
 );
 
-static const __m256i m256_intersection_lut_12 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_12 = _mm256_set_epi8(
     2,2,2,2,1,1,1,1,1,1,1,1,0,0,0,0,
     2,2,2,2,1,1,1,1,1,1,1,1,0,0,0,0
 );
 
-static const __m256i m256_intersection_lut_13 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_13 = _mm256_set_epi8(
     3,2,3,2,2,1,2,1,2,1,2,1,0,1,0,1,
     3,2,3,2,2,1,2,1,2,1,2,1,0,1,0,1
 );
 
-static const __m256i m256_intersection_lut_14 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_14 = _mm256_set_epi8(
     3,3,2,2,2,2,1,1,2,2,1,1,1,1,0,0,
     3,3,2,2,2,2,1,1,2,2,1,1,1,1,0,0
 );
 
-static const __m256i m256_intersection_lut_15 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_intersection_lut_15 = _mm256_set_epi8(
     4,3,3,2,3,2,2,1,3,2,2,1,1,0,1,0,
     4,3,3,2,3,2,2,1,3,2,2,1,1,0,1,0
 );
 
 
-static const __m256i m256_union_lut_0  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_0  = _mm256_set_epi8(
     4, 3, 3, 2, 3, 2, 2, 1, 3, 2, 2, 1, 2, 1, 1, 0,
     4, 3, 3, 2, 3, 2, 2, 1, 3, 2, 2, 1, 2, 1, 1, 0
 );
 
-static const __m256i m256_union_lut_1  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_1  = _mm256_set_epi8(
     4, 4, 3, 3, 3, 3, 2, 2, 4, 4, 3, 3, 3, 3, 1, 1,
     4, 4, 3, 3, 3, 3, 2, 2, 4, 4, 3, 3, 3, 3, 1, 1
 );
 
-static const __m256i m256_union_lut_2  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_2  = _mm256_set_epi8(
     4, 3, 4, 3, 3, 2, 3, 2, 3, 2, 3, 2, 2, 1, 2, 1,
     4, 3, 4, 3, 3, 2, 3, 2, 3, 2, 3, 2, 2, 1, 2, 1
 );
 
-static const __m256i m256_union_lut_3  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_3  = _mm256_set_epi8(
     4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2,
     4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2
 );
 
-static const __m256i m256_union_lut_4  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_4  = _mm256_set_epi8(
     4, 3, 3, 2, 4, 3, 3, 2, 3, 4, 3, 2, 3, 2, 2, 1,
     4, 3, 3, 2, 4, 3, 3, 2, 3, 4, 3, 2, 3, 2, 2, 1
 );
 
-static const __m256i m256_union_lut_5  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_5  = _mm256_set_epi8(
     4, 4, 3, 3, 4, 4, 3, 3, 3, 3, 2, 2, 3, 3, 2, 2,
     4, 4, 3, 3, 4, 4, 3, 3, 3, 3, 2, 2, 3, 3, 2, 2
 );
 
-static const __m256i m256_union_lut_6  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_6  = _mm256_set_epi8(
     4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 3, 2, 3, 2,
     4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 3, 2, 3, 2
 );
 
-static const __m256i m256_union_lut_7  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_7  = _mm256_set_epi8(
     4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3,
     4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3
 );
 
-static const __m256i m256_union_lut_8  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_8  = _mm256_set_epi8(
     4, 3, 3, 2, 4, 3, 3, 2, 3, 2, 2, 1, 3, 2, 2, 1,
     4, 3, 3, 2, 4, 3, 3, 2, 3, 2, 2, 1, 3, 2, 2, 1
 );
 
-static const __m256i m256_union_lut_9  = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_9  = _mm256_set_epi8(
     4, 4, 3, 3, 4, 4, 3, 3, 3, 3, 2, 2, 3, 3, 2, 2,
     4, 4, 3, 3, 4, 4, 3, 3, 3, 3, 2, 2, 3, 3, 2, 2
 );
 
-static const __m256i m256_union_lut_10 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_10 = _mm256_set_epi8(
     4, 3, 4, 3, 4, 3, 3, 2, 3, 2, 3, 2, 4, 3, 3, 2,
     4, 3, 4, 3, 4, 3, 3, 2, 3, 2, 3, 2, 4, 3, 3, 2
 );
 
-static const __m256i m256_union_lut_11 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_11 = _mm256_set_epi8(
     4, 4, 4, 4, 3, 3, 3, 3, 4, 4, 4, 4, 3, 3, 3, 3,
     4, 4, 4, 4, 3, 3, 3, 3, 4, 4, 4, 4, 3, 3, 3, 3
 );
 
-static const __m256i m256_union_lut_12 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_12 = _mm256_set_epi8(
     4, 3, 3, 2, 4, 3, 3, 2, 4, 3, 3, 2, 4, 3, 3, 2,
     4, 3, 3, 2, 4, 3, 3, 2, 4, 3, 3, 2, 4, 3, 3, 2
 );
 
-static const __m256i m256_union_lut_13 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_13 = _mm256_set_epi8(
     4, 4, 3, 3, 4, 4, 3, 3, 4, 4, 3, 3, 4, 4, 3, 3,
     4, 4, 3, 3, 4, 4, 3, 3, 4, 4, 3, 3, 4, 4, 3, 3
 );
 
-static const __m256i m256_union_lut_14 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_14 = _mm256_set_epi8(
     4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3,
     4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3
 );
 
-static const __m256i m256_union_lut_15 = _mm256_set_epi8(
+alignas(64) static const __m256i m256_union_lut_15 = _mm256_set_epi8(
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
 );
 
 
-static const __m256i m256_intersection_lookup_tables[16] = {
+alignas(64) static const __m256i m256_intersection_lookup_tables[16] = {
     m256_intersection_lut_0, m256_intersection_lut_1, m256_intersection_lut_2, m256_intersection_lut_3,
     m256_intersection_lut_4, m256_intersection_lut_5, m256_intersection_lut_6, m256_intersection_lut_7,
     m256_intersection_lut_8, m256_intersection_lut_9, m256_intersection_lut_10, m256_intersection_lut_11,
     m256_intersection_lut_12, m256_intersection_lut_13, m256_intersection_lut_14, m256_intersection_lut_15
 };
 
-static const __m256i m256_union_lookup_tables[16] = {
+alignas(64) static const __m256i m256_union_lookup_tables[16] = {
     m256_union_lut_0, m256_union_lut_1, m256_union_lut_2, m256_union_lut_3,
     m256_union_lut_4, m256_union_lut_5, m256_union_lut_6, m256_union_lut_7,
     m256_union_lut_8, m256_union_lut_9, m256_union_lut_10, m256_union_lut_11,
