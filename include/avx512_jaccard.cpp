83ec58bdcfebdde271c7bb76b35c20015bcd918e
lkuffo <lxkr@cwi.nl>
2025-05-29 16:41:15 +0200

    Optimizing access to LUTs

--- include/avx512_jaccard.cpp
+++ include/avx512_jaccard.cpp
@@ -218,9 +218,6 @@ void jaccard_b256_vpshufb_precomputed_pdx(
         uint8_t first_high = (first_vector[dim] & 0xF0) >> 4;
         uint8_t first_low = first_vector[dim] & 0x0F;
 
-        uint8_t next_dim_high = (first_vector[dim+1] & 0xF0) >> 4;
-        uint8_t next_dim_low = first_vector[dim+1] & 0x0F;
-
         // Choose lookup tables
 //        __m256i lut_intersection_high = _mm256_loadu_epi8((__m256i const*)(&static_intersection_lookup_tables[0]));
 //        __m256i lut_intersection_low  = _mm256_loadu_epi8((__m256i const*)(&static_intersection_lookup_tables[1]));
@@ -242,10 +239,6 @@ void jaccard_b256_vpshufb_precomputed_pdx(
             intersections_result[i] = _mm256_add_epi8(intersections_result[i], intersection);
 
             second_vector += 32; // 256x8-bit values (using 8 registers at a time)
-            if (i == 6){
-                __builtin_prefetch(&m256_intersection_lookup_tables[next_dim_high], 0, 3);
-                __builtin_prefetch(&m256_intersection_lookup_tables[next_dim_low], 0, 3);
-            }
         }
     }
     // TODO: Ugly
