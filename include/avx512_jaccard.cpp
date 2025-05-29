08aed79a5ad30fb66243829f31683a4ad36778b9
lkuffo <lxkr@cwi.nl>
2025-05-29 13:10:20 +0200

    Shuffle with 2 different accumulators for every nibble

--- include/avx512_jaccard.cpp
+++ include/avx512_jaccard.cpp
@@ -219,7 +219,7 @@ void jaccard_b256_vpshufb_precomputed_pdx(
 
         // Choose lookup tables
         //__m256i lut_intersection_high = m256_intersection_lookup_tables[first_high];
-        __m256i lut_intersection_low = m256_intersection_lookup_tables[0];
+        __m256i lut_intersection_low = m256_intersection_lookup_tables[first_low];
 
         for (size_t i = 0; i < 8; i++){ // 256 uint8_t values
             __m256i second = _mm256_loadu_epi8((__m256i const*)(second_vector));
