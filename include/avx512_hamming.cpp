29ddd11b2a58818dd98fc288a89a7262dcef02c6
lkuffo <lxkr@cwi.nl>
2025-05-30 15:58:12 +0200

    Trying XOR LUTs

--- include/avx512_hamming.cpp
+++ include/avx512_hamming.cpp
@@ -136,8 +136,8 @@ void hamming_b256_xorlut_pdx(uint8_t const *first_vector, uint8_t const *second_
         uint8_t first_high = (first_vector[dim] & 0xF0) >> 4;
         uint8_t first_low = first_vector[dim] & 0x0F;
         // Choose lookup tables
-        __m512i lut_xor_high = m512_xor_lookup_tables[first_high];
-        __m512i lut_xor_low = m512_xor_lookup_tables[0];
+        __m512i lut_xor_high = m512_xor_lookup_tables[dim % 16];
+        __m512i lut_xor_low = m512_xor_lookup_tables[dim % 16];
 
         for (size_t i = 0; i < 4; i++){ // 256 uint8_t values
             __m512i second = _mm512_loadu_epi8(second_vector);
