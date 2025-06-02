4cb7e3ae6b79cf4a96a0fb84ad550b5215f03f26
lkuffo <lxkr@cwi.nl>
2025-06-02 10:53:32 +0200

    Sequential LUT XOR

--- include/avx512_hamming.cpp
+++ include/avx512_hamming.cpp
@@ -143,16 +143,16 @@ void hamming_b256_xorlut_pdx(uint8_t const *first_vector, uint8_t const *second_
     }
 
     for (size_t dim = 0; dim != 32; dim++){
-        uint8_t first_high = (first_vector[dim] & 0xF0) >> 4;
-        uint8_t first_low = first_vector[dim] & 0x0F;
-        __m512i lut_xor_high = m512_xor_lookup_tables[first_high];
-        __m512i lut_xor_low = m512_xor_lookup_tables[first_low];
+//        uint8_t first_high = (first_vector[dim] & 0xF0) >> 4;
+//        uint8_t first_low = first_vector[dim] & 0x0F;
+//        __m512i lut_xor_high = m512_xor_lookup_tables[first_high];
+//        __m512i lut_xor_low = m512_xor_lookup_tables[first_low];
 
 //        __m512i lut_xor_high = _mm512_loadu_epi8(query_aware_b256_xorluts_avx512 + ((dim * 2) * 64));
 //        __m512i lut_xor_low = _mm512_loadu_epi8(query_aware_b256_xorluts_avx512 + (((dim * 2) + 1) * 64));
 
-//        __m512i lut_xor_high = _mm512_broadcast_i32x4(_mm_load_si128(reinterpret_cast<const __m128i*>(query_aware_b256_xorluts_high + (dim * 16))));
-//        __m512i lut_xor_low = _mm512_broadcast_i32x4(_mm_load_si128(reinterpret_cast<const __m128i*>(query_aware_b256_xorluts_low + (dim * 16))));
+        __m512i lut_xor_high = _mm512_broadcast_i32x4(_mm_load_si128(reinterpret_cast<const __m128i*>(query_aware_b256_xorluts_high + (dim * 16))));
+        __m512i lut_xor_low = _mm512_broadcast_i32x4(_mm_load_si128(reinterpret_cast<const __m128i*>(query_aware_b256_xorluts_low + (dim * 16))));
 
         for (size_t i = 0; i < 4; i++){ // 256 uint8_t values
             __m512i second = _mm512_loadu_epi8(second_vector);
